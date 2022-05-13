97353f0ea51fe9e39980089e4f4492366f979506
Qiong Qin <zoeqin03@gmail.com>
2022-05-13 13:58:25 -0400

    this drive me crazy

---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	// fmt.Println("hello I am testing how to import packages")
+
+	// type qq struct{
+	//     QQ string;
+	// }
+
+	// func (*qq) eat() {
+	// 	fmt.Println("she eats")
+	// }
+	// func (*qq) drink() {
+	// 	fmt.Println("she drinks")
+	// }
+
+	// func (*qq) drink() {
+	// 	fmt.Println("she drinks")
+	// }
+
+	// newWoman := NewPerson(&man)
+	person1.hello
+
+}
---
+++ person1/person.go
@@ -0,0 +1,23 @@
+package person1
+
+import "fmt"
+
+// type person struct {
+// 	todo behavior
+// }
+
+// type behavior interface {
+// 	eat()
+// 	drink()
+// 	sleep()
+// }
+
+// func NewPerson(be behavior) *person {
+// 	return &person{
+// 		todo: be,
+// 	}
+// }
+
+func hello() {
+	fmt.Println("hello")
+}
